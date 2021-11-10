430109b240ea56a0ea6a49408064ec5e6a346f65
Yogesh Narendra Tiwari <92584567+yogeshnarendratiwari@users.noreply.github.com>
2021-11-10 15:50:38 +0530

    Button

---
+++ java/com/oslac/button/MainActivity.java
@@ -0,0 +1,26 @@
+package com.oslac.button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+    public void Action(View v){
+
+    }
+}
